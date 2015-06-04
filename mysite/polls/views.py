234f4021afbecb065503fba048565dff17e76db1
alamok <q2ckwc0@gmail.com>
2015-06-04 10:10:13 +0530

    Adding few views in polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. you are at the index page")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+        })
+    #output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(template.render(context))
 
+def detail(request, question_id):
+    return HttpResponse("you are looking at the results of question %s" % question_id)
+
+def results(request, question_id):
+    response = "you are looking at the results of question %s"
+    return HttpResponse(response % question_id )
+
+def vote(request, question_id):
+    return HttpResponse("you are voting for the question %s." % question_id)
