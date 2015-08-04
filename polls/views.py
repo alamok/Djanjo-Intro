e9d85c175d774086704053c9fc47b01f15f86b20
alamok <q2ckwc0@gmail.com>
2015-08-04 20:03:32 +0530

    adding elasticsearch

--- polls/views.py
+++ polls/views.py
@@ -4,11 +4,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from elasticsearch import Elasticsearch as ES
 
 from .models import Question
 
 
 def index(request):
+    # test a elastic search connection and create index 
+    # if index already exists dont do anything
+
+    es = ES('http://127.0.0.1:9200/')
+    es.indices.create(index='test-rule1', ignore=400)
+    # we may need to create a function here to hold another template in case elastic search
+    # does not load.
+
+    
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = RequestContext(request, {
